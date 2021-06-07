be2bd01bcfb50ff7410d89a2e22607ea39ba8201
Tela Caul <telacaul@gmail.com>
2021-06-06 19:17:59 -0700

    revert app listen

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
+const sequelize = require('./config/connection');
 // import sequelize connection
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
